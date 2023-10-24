91fd8760dc3310dcb40d40d105e56cb77bef9247
Alexeykaa <ka.alexey@gmail.com>
2023-10-24 10:29:51 +0300

    init implementation

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        boolean result = false;
+        try {
+            Number n = NumberUtils.createNumber(str);
+            result = n != null && n.doubleValue() > 0;
+        } catch (NumberFormatException ignoreIt) {
+            // result is false
+        }
+        return result;
     }
 }
